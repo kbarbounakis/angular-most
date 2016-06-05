6359a221b15db9121021b5daf11d6fa6d6b79f05
kbarbounakis <k.barbounakis@gmail.com>
2016-06-05 23:13:22 +0300

    adds ItemController scope.getContext() method

--- angular-most.js
+++ angular-most.js
@@ -29,6 +29,7 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+(function(window, angular, $) {
 
 // Create Base64 Object
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
@@ -2372,6 +2373,7 @@ function CommonController($scope, $q, $location, $window, $shared) {
 
     //find first element with ng-scope (root element)
     var $route,
+        $context,
         $routeParams,
         $stateParams,
         $rootElement = angular.element(document.querySelector('.ng-scope')), $injector = $rootElement.injector();
@@ -2392,7 +2394,18 @@ function CommonController($scope, $q, $location, $window, $shared) {
             $route = $injector.get('$route');
         if ($injector.has('$shared'))
             $shared = $shared || $injector.get('$shared');
+        if ($injector.has('$context'))
+            $context = $injector.get('$context');
     }
+
+    /**
+     * Gets an instance of ClientDataContext service.
+     * @returns {ClientDataContext}
+     */
+    $scope.getContext = function() {
+        Args.notNull($context, "MOST Data Context Service");
+        return $context;
+    };
 
     //register broadcast emitter
     $scope.broadcast = function(name, args) {
@@ -3623,4 +3636,18 @@ most.directive('loc',MostLocalizedDirective)
     .directive('mostEmail',MostEmailDirective);
 //MODULE FILTERS
 most.filter('loc', MostLocalizedFilter);
+
+if (window) {
+    //export prototypes
+    window.ClientDataQueryable = ClientDataQueryable;
+    window.ClientDataService = ClientDataService;
+    window.ClientDataModel = ClientDataModel;
+    window.MostDataField = MostDataField;
+    //export controllers
+    window.CommonController = CommonController;
+    window.DataController = DataController;
+    window.ItemController = ItemController;
+    window.QueryableController = QueryableController;
+}
+})(window, angular, jQuery);
 
