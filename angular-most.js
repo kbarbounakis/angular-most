b51cecfcd416b73cdcfd968a6da25e609fbbe6dc
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2016-10-12 16:48:51 +0300

    removes obsolete jQuery reference

--- angular-most.js
+++ angular-most.js
@@ -29,7 +29,7 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-(function(window, angular, $) {
+(function(window, angular) {
 
 // Create Base64 Object
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
@@ -3189,13 +3189,16 @@ function MostEmailDirective($compile) {
         replace:true,
         compile: function compile(tElement, tAttrs) {
             return function (scope, iElement, iAttrs) {
-                var $element = $(iElement);
+                var $element = angular.element(iElement);
                 $element.find("label").html(iAttrs.title);
                 var attributes = $element.prop("attributes");
                 var $input = $element.find("input");
-                $.each(attributes, function () {
-                    if (this.name !== "class") { $input.attr(this.name, this.value); }
-                });
+                if (angular.isArray(attributes)) {
+                    for (var i = 0; i < attributes.length; i++) {
+                        var obj = attributes[i];
+                        if (obj.name !== "class") { $input.attr(obj.name, obj.value); }
+                    }
+                }
                 $compile($element)(scope);
             };
         }
@@ -3209,13 +3212,16 @@ function MostStringDirective($compile) {
         replace:true,
         compile: function compile(tElement, tAttrs) {
             return function (scope, iElement, iAttrs) {
-                var $element = $(iElement);
+                var $element = angular.element(iElement);
                 $element.find("label").html(iAttrs.title);
                 var attributes = $element.prop("attributes");
                 var $input = $element.find("input");
-                $.each(attributes, function () {
-                    if (this.name !== "class") { $input.attr(this.name, this.value); }
-                });
+                if (angular.isArray(attributes)) {
+                    for (var i = 0; i < attributes.length; i++) {
+                        var obj = attributes[i];
+                        if (obj.name !== "class") { $input.attr(obj.name, obj.value); }
+                    }
+                }
                 $compile($element)(scope);
             };
         }
@@ -3229,7 +3235,7 @@ function MostTypeaheadDirective($compile, $svc) {
         replace:true,
         compile: function compile() {
             return function (scope, iElement, iAttrs) {
-                var $element = $(iElement);
+                var $element = angular.element(iElement);
                 //set label
                 if (typeof iAttrs.title!=='undefined')
                     $element.find("label").html(iAttrs.title);
@@ -3241,17 +3247,19 @@ function MostTypeaheadDirective($compile, $svc) {
                 var dataModel = iAttrs.model;
                 var attributes = $element.prop("attributes");
                 var $input = $element.find("input");
-                $.each(attributes, function () {
-                    if (this.name === "placeholder") {
-                        $input.attr(this.name,angular.localized(this.value));
-                    }
-                    else {
-                        if (this.name !== "class") {
-                            $input.attr(this.name, this.value);
+
+                if (angular.isArray(attributes)) {
+                    for (var k = 0; i < attributes.length; k++) {
+                        var obj = attributes[k];
+                        if (obj.name === "placeholder") {
+                            $input.attr(obj.name,angular.localized(obj.value));
+                        }
+                        else if (obj.name !== "class") {
+                            $input.attr(obj.name, obj.value);
                         }
                     }
-                });
-                //set typeahead properties
+                }
+                //set type ahead properties
                 var field = iAttrs['field'], dataFilter=null;
                 var value = iAttrs['value'] || "x";
                 var typeaheadAttr = angular.format('%s as x.%s for x in %s($viewValue)',value, field, dataName);
@@ -3723,5 +3731,5 @@ if (window) {
     window.ItemController = ItemController;
     window.QueryableController = QueryableController;
 }
-})(window, angular, jQuery);
+})(window, angular);
 
